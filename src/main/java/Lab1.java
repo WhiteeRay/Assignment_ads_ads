a3ce05b55f25d2b697ed8b52ac79fee4b0542ba0
White ray <white.ray.haha@gmail.com>
2025-03-29 14:34:35 +0500

    Problem 1

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -14,11 +14,20 @@ public class Lab1 {
 
 
         System.out.println(findMinimumOfTheArray(n,arr));
+        System.out.println(findAverageOfAnArray(n,arr));
         sc.close();
 
 
     }
 
+    /*
+          Finds the minimum element in the given array.
+          @param n   the size of the array
+          @param arr the array containing elements
+          @return the minimum element in the array
+          Time Complexity: O(n) (Linear Time Complexity),
+          as we iterate through the array once.
+         */
     public static int findMinimumOfTheArray(int n, int[] arr){
         int min =arr[0];
         for(int i = 1;i < n;i++){
@@ -27,15 +36,26 @@ public class Lab1 {
             }
         }
         return min;
-
-
-        /*
-          Finds the minimum element in the given array.
-          @param n   the size of the array
-          @param arr the array containing elements
-          @return the minimum element in the array
-          Time Complexity: O(n), where n is the number of elements in the array
-          The function iterates through the array once, making it linear in complexity.
-         */
     }
+
+
+    /*
+      Calculates the average of an array
+      @param n   the size of the array
+      @param arr the array containing elements
+      @return the average value as a double
+      Time Complexity: O(n), (Linear Time Complexity)
+      as we iterate through the array once to calculate the sum.
+     */
+    public static double findAverageOfAnArray(int n, int[] arr){
+        int sum =0;
+        for(int num : arr){
+            sum+=num;
+        }
+        return (double) sum/n;
+    }
+
+
+
+
 }
