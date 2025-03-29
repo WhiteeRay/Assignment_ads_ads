e7b1f4524c0fcaacd6e8b4481d00e6a12b6ccb0c
White ray <white.ray.haha@gmail.com>
2025-03-29 14:23:02 +0500

    Problem 1

---
+++ src/main/java/Lab1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i =0; i<n;i++){
+            arr[i] =sc.nextInt();
+        }
+
+
+
+
+        System.out.println(findMinimumOfTheArray(n,arr));
+        sc.close();
+
+
+    }
+
+    public static int findMinimumOfTheArray(int n, int[] arr){
+        int min =arr[0];
+        for(int i = 1;i < n;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+
+
+        /*
+          Finds the minimum element in the given array.
+          @param n   the size of the array
+          @param arr the array containing elements
+          @return the minimum element in the array
+          Time Complexity: O(n), where n is the number of elements in the array
+          The function iterates through the array once, making it linear in complexity.
+         */
+    }
+}
